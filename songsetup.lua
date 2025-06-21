4e9626d1e04f05f26c2e07f1bc30d084a5a5110d
Steve Arch <steve@arch>
2025-06-21 17:00:34 -0400

    feat: better interface and global uservars

--- songsetup.lua
+++ songsetup.lua
@@ -9,97 +9,120 @@ Version: 1.0
 ==============================================================================
 ]]
 
+local uservars = {
+	user_suffix = "_vx",
+	fixed_page_name = "FixedPage",
+	show_exec_name = "ShowExec",
+	page_turn_name = "PageTurn",
+	page_turn_macro_number = "",
+	view_screen = "",
+	main_executor = "29",
+	objects_to_setup = { "Sequence", "Macro", "Page", "Timecode", "View" },
+}
+
 local function validate_input(_input)
-  if _input == nil or _input == "" then
-    gma.feedback("Plugin cancelled by user")
-    return false
-  end
-  return true
+	if _input == nil or _input == "" then
+		gma.feedback("Plugin cancelled by user")
+		return false
+	end
+	return true
 end
 
 local function get_label(_id)
-  return gma.show.getobj.label(gma.show.getobj.handle(_id))
+	return gma.show.getobj.label(gma.show.getobj.handle(_id))
 end
 
-local function create_pool_item(
-    amount --[[int]],
-    start_number --[[int]],
-    pool_id --[[string]],
-    view_screen --[[string]],
-    vx_exec --[[string]]
-
+local function cmd(
+	template --[[string]],
+	args --[[table]]
 )
-  local j = 1
-  for i = start_number, start_number + amount do
-    if pool_id:find("View") == nil then
-      gma.cmd("store " .. pool_id .. " " .. i .. " Song" .. j .. " /o /nc")
-    else
-      gma.cmd("store " .. pool_id .. " " .. i .. " /screen=" .. view_screen)
-      gma.cmd("assign " .. pool_id .. " " .. i .. " /name=" .. "Song" .. j)
-    end
-    if pool_id:find("Page") ~= nil then
-      gma.cmd("assign seq " .. i .. " at exec " .. i .. "." .. vx_exec)
-    end
-    if pool_id:finc("Macro") ~= nil then
-      gma.cmd("store macro 1." .. i .. ".1 thru 2")
-      -- store song specific cmdline
-    end
-
-    j = j + 1
-  end
-  --
+	gma.feedback(template)
+	gma.feedback(table.unpack(args))
+	local s = string.format(template, table.unpack(args))
+	gma.feedback("Sending cmd", s)
+	gma.cmd(s)
+	return nil
 end
 
-
-
--- Label Macro
--- Label Sequence
--- Label Page
--- Label Timecode
--- Label View
-
+local function create_pool_items(
+	amount --[[int]],
+	start_number --[[int]]
+)
+	local j = 1
+	for i = start_number, start_number + amount do
+		local song = "Song" .. j .. uservars.user_suffix
+		for _, pool_id in pairs(uservars.objects_to_setup) do
+			if pool_id:lower() ~= "view" then
+				cmd('store %s %d "%s" /o /nc', { pool_id, i, song })
+				if pool_id:lower() == "page" then
+					cmd("assign seq %d at exec %d.%d", { i, i, uservars.main_executor })
+				end
+				if pool_id:lower() == "macro" then
+					cmd("store macro 1.%d.1 thru 2", { i })
+				end
+			end
+		end
+		cmd("store view %d /screen=%d", { i, uservars.view_screen })
+		cmd('assign view %d /name="%s"', { i, song })
+		j = j + 1
+	end
+	return nil
+end
 
 local function main()
-  gma.feedback("Starting song setup plugin ... ")
+	gma.feedback("Starting song setup plugin ... ")
 
-  local song_amount_str = gma.textinput("Enter the amount of songs to setup:", "50")
-  if validate_input(song_amount_str) == false then return end
+	-- get user input
+	local song_amount_str = gma.textinput("Enter the amount of songs to setup:", "50")
+	if validate_input(song_amount_str) == false then
+		return
+	end
 
-  local first_pool_item_str = gma.textinput("Enter the starting pool item", "6001")
-  if validate_input(first_pool_item_str) == false then return end
+	local first_pool_item_str = gma.textinput("Enter the starting pool item", "6001")
+	if validate_input(first_pool_item_str) == false then
+		return
+	end
 
-  local vx_exec = gma.textinput("Enter the video show executor number", "29")
-  if validate_input(vx_exec) == false then return end
+	local user_exec = gma.textinput("Enter the main executor number", "29")
+	if validate_input(user_exec) == false then
+		return
+	end
+	uservars.main_executor = user_exec
 
-  local view_screen = gma.textinput("Which screen shall we create the views on?", "3")
-  if validate_input(view_screen) == false then return end
+	local view_screen = gma.textinput("Which screen shall we create the views on?", "3")
+	if validate_input(view_screen) == false then
+		return
+	end
 
-  local page_turn_macro = gma.textinput("At what macro number shall we create the page turn macro?", "6000")
-  if validate_input(view_screen) == false then return end
+	uservars.view_screen = view_screen
 
-  local fixed_page = gma.textinput("What page shall be your fixed executors page?", "6000")
-  if validate_input(fixed_page) == false then return end
+	local page_turn_macro = gma.textinput("At what macro number shall we create the page turn macro?", "6000")
+	if validate_input(view_screen) == false then
+		return
+	end
 
-  gma.cmd("store page " .. fixed_page .. ' "Vx_Fixed"')
-  gma.user.setvar("PhilFixed", fixed_page)
-  gma.cmd("store macro " .. page_turn_macro .. "/name=PageTurn")
-  gma.cmd("store macro 1.PageTurn.1 thru 4")
+	uservars.page_turn_macro = page_turn_macro
 
-  -- str to int all the things
-  local song_amount = tonumber(song_amount_str)
-  local first_pool_item = tonumber(first_pool_item_str)
+	local fixed_page = gma.textinput("What page shall be your fixed executors page?", "6000")
+	if validate_input(fixed_page) == false then
+		return
+	end
 
-  gma.user.setvar("PhilShowExec", vx_exec)
+	-- static stuff
+	gma.feedback("Setting up fixed page and pageturn macro")
+	gma.user.setvar(uservars.fixed_page_name, fixed_page)
+	gma.user.setvar(uservars.show_exec_name, user_exec)
+	cmd("store page %d %s", { fixed_page, uservars.fixed_page_name .. uservars.user_suffix })
+	cmd('store macro %d "%s"', { uservars.page_turn_macro, uservars.page_turn_name .. uservars.user_suffix })
+	-- check if a page turn already exists with the same name?
+	cmd("store macro 1.%d.1 thru 4", { uservars.page_turn_macro })
 
-  create_pool_item(song_amount, first_pool_item, "Sequence", view_screen, vx_exec)
-  create_pool_item(song_amount, first_pool_item, "Macro", view_screen, vx_exec)
-  create_pool_item(song_amount, first_pool_item, "Page", view_screen, vx_exec)
-  create_pool_item(song_amount, first_pool_item, "Timecode", view_screen, vx_exec)
-  create_pool_item(song_amount, first_pool_item, "View", view_screen, vx_exec)
+	-- str to int all the things
+	local song_amount = tonumber(song_amount_str)
+	local first_pool_item = tonumber(first_pool_item_str)
 
-
-
-
-  --
+	-- create all the stuff
+	gma.feedback("Starting to create the per song objects")
+	create_pool_items(song_amount, first_pool_item)
 end
 return main
