04788c2b49a9b0339f0d675f88eaf9e3e80bf850
Steve Arch <steve@arch>
2025-06-22 02:36:06 -0400

    feat: plugin is working

---
+++ common.lua
@@ -0,0 +1,38 @@
+return {
+
+	uservars = {
+		user_suffix = "_vx",
+		fixed_page_name = "FixedPage",
+		fixed_pool_item_number = 5001,
+		show_exec_name = "ShowExec",
+		page_turn_name = "PageTurn",
+		page_turn_macro_number = "",
+		view_screen = "",
+		main_executor = "29",
+		objects_to_setup = { "Sequence", "Macro", "Page", "Timecode", "View" },
+	},
+
+	get_label = function(_id)
+		return gma.show.getobj.label(gma.show.getobj.handle(_id))
+	end,
+
+	validate_input = function(_input)
+		if _input == nil or _input == "" then
+			gma.feedback("Plugin cancelled by user")
+			return false
+		end
+		return true
+	end,
+
+	cmd = function(
+		template --[[string]],
+		args --[[table]]
+	)
+		gma.feedback(template)
+		gma.feedback(table.unpack(args))
+		local s = string.format(template, table.unpack(args))
+		gma.feedback("Sending cmd :", s)
+		gma.cmd(s)
+		return nil
+	end,
+}
--- songsetup.lua
+++ songsetup.lua
@@ -1,5 +1,12 @@
 require("gma_api")
 
+local common = require("common")
+local uservars = common.uservars
+local cmd = common.cmd
+local validate_input = common.validate_input
+
+local view = require("view_items")
+
 --[[
 ==============================================================================
 grandMA2 LUA Plugin: Song Setup
@@ -9,39 +16,33 @@ Version: 1.0
 ==============================================================================
 ]]
 
-local uservars = {
-	user_suffix = "_vx",
-	fixed_page_name = "FixedPage",
-	show_exec_name = "ShowExec",
-	page_turn_name = "PageTurn",
-	page_turn_macro_number = "",
-	view_screen = "",
-	main_executor = "29",
-	objects_to_setup = { "Sequence", "Macro", "Page", "Timecode", "View" },
-}
-
-local function validate_input(_input)
-	if _input == nil or _input == "" then
-		gma.feedback("Plugin cancelled by user")
-		return false
+local function create_song_view(song_name, song_pool_number, song_number)
+	local xmlfile = view.getXML(
+		song_name,
+		uservars.fixed_pool_item_number,
+		song_pool_number,
+		song_pool_number - 1 + song_number * 62
+	)
+	local xmlpath = gma.show.getvar("path") .. "/importexport/"
+	local current_view = "view_" .. song_name
+	local filename = xmlpath .. current_view .. ".xml"
+	local file, err = io.open(filename, "w")
+	if not file then
+		err = "Error opening file: " .. err
+		return false, err
 	end
-	return true
-end
+	file:write(xmlfile)
+	file:close()
+	gma.cmd("selectdrive 1")
+	cmd('import "%s" at view %s /o /nc', { current_view, song_pool_number })
+	gma.cmd("delete screen " .. uservars.view_screen)
+	local view_cmd = "view " .. song_pool_number .. " /screen=" .. uservars.view_screen
+	gma.cmd(view_cmd)
+	gma.sleep(0.1)
+	gma.cmd("store " .. view_cmd)
 
-local function get_label(_id)
-	return gma.show.getobj.label(gma.show.getobj.handle(_id))
-end
-
-local function cmd(
-	template --[[string]],
-	args --[[table]]
-)
-	gma.feedback(template)
-	gma.feedback(table.unpack(args))
-	local s = string.format(template, table.unpack(args))
-	gma.feedback("Sending cmd", s)
-	gma.cmd(s)
-	return nil
+	os.remove(filename)
+	return true, nil
 end
 
 local function create_pool_items(
@@ -49,7 +50,7 @@ local function create_pool_items(
 	start_number --[[int]]
 )
 	local j = 1
-	for i = start_number, start_number + amount do
+	for i = start_number, start_number + amount - 1 do
 		local song = "Song" .. j .. uservars.user_suffix
 		for _, pool_id in pairs(uservars.objects_to_setup) do
 			if pool_id:lower() ~= "view" then
@@ -62,8 +63,13 @@ local function create_pool_items(
 				end
 			end
 		end
-		cmd("store view %d /screen=%d", { i, uservars.view_screen })
-		cmd('assign view %d /name="%s"', { i, song })
+		local r, err = create_song_view(song, i, j - 1)
+		if not r then
+			gma.feedback(err)
+			break
+		end
+		-- cmd("store view %d /screen=%d", { i, uservars.view_screen })
+		-- cmd('assign view %d /name="%s"', { i, song })
 		j = j + 1
 	end
 	return nil
@@ -78,7 +84,13 @@ local function main()
 		return
 	end
 
-	local first_pool_item_str = gma.textinput("Enter the starting pool item", "6001")
+	local fix_pool_item_str = gma.textinput("Enter the fixed pool item number", "5001")
+	if validate_input(fix_pool_item_str) == false then
+		return
+	end
+	uservars.fixed_pool_item_number = tonumber(fix_pool_item_str) - 1
+
+	local first_pool_item_str = gma.textinput("Enter the starting songs pool item", "6001")
 	if validate_input(first_pool_item_str) == false then
 		return
 	end
