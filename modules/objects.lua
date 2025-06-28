f7bd04eb9808c1e5063ff3aad5fbe164e2fac46c
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-06-28 00:03:23 -0400

    added create object function

--- modules/objects.lua
+++ modules/objects.lua
@@ -4,11 +4,12 @@
 ---@field number? number either 15 as in group 15 or 15.14 as in preset 15.14 or 1.1.1 as in macroline
 ---@field setlabel fun(self: table, label: string)
 ---@field get_handle fun(self: table): handle | nil
+---@field create fun(self: table)
 _M = {}
 
 _M.setlabel = function(self, label)
 	if self.number == nil then
-		gma.gui.msgbox("Error", "Missing field to properly identify object")
+		gma.gui.msgbox("Error", "Missing number field to properly identify and label object")
 		return
 	end
 	self.label = label
@@ -18,7 +19,7 @@ end
 
 _M.get_handle = function(self)
 	if self.label == nil and self.number == nil then
-		gma.gui.msgbox("Error", "Missing field(s) to properly identify object")
+		gma.gui.msgbox("Error", "Missing field(s) to properly identify object and get handle")
 		return
 	end
 	if self.number then
@@ -29,5 +30,16 @@ _M.get_handle = function(self)
 	end
 end
 
----@type GMAObject
+_M.create = function(self)
+	local cmd = ""
+	if self.label then
+		cmd = string.format('store %s %d "%s"', self.type, self.number, self.label)
+	elseif self.number then
+		cmd = string.format("store %s %d", self.type, self.number)
+	else
+		gma.gui.msgbox("Error", "Missing field to properly store object")
+	end
+	gma.cmd(cmd)
+end
+
 return _M
