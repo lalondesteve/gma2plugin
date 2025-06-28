65f9f01b2b43a04b3f090ec18c72e396afe5c7b7
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-06-28 00:18:47 -0400

    proper indentation to setclipspeed

--- setclipspeed.lua
+++ setclipspeed.lua
@@ -1,11 +1,10 @@
 function set_clip_speed(clip_tempo)
-if tonumber(clip_tempo) == nil then
-gma.gui.msgbox("Invalid Input", "You must enter a numeric value")
-return
-end
+	if tonumber(clip_tempo) == nil then
+		gma.gui.msgbox("Invalid Input", "You must enter a numeric value")
+		return
+	end
 
-local speed = gma.user.getvar('PhilTempo')/clip_tempo
-gma.feedback('Speed: ', speed)
-gma.cmd('Attribute "speed" at '..speed)
-
+	local speed = gma.user.getvar("PhilTempo") / clip_tempo
+	-- gma.feedback('Speed: ', speed)
+	gma.cmd('Attribute "speed" at ' .. speed)
 end
