30671527d79253345bcf9c60836caa9bf9e01c5b
steve <steve@luzstudio.net>
2025-07-03 16:43:06 -0400

    added rename from macro option

--- renamesongs.lua
+++ renamesongs.lua
@@ -1,4 +1,5 @@
 function rename_songs()
+	local user_suffix = " _vx"
 	local objects_to_rename = {
 		"seq",
 		"macro",
@@ -11,7 +12,7 @@ function rename_songs()
 	local source = gma.textinput("Rename from sequence or macro? (write s or m)")
 	source = source:lower()
 	if source and source ~= "s" or source ~= "m" then
-		gma.gui.msgbox("Error", "Bad input provided, only the letters s and m are allowed")
+		gma.gui.msgbox("Error", "Bad input provided, only the lowercase letters s and m are allowed")
 	else
 		return
 	end
@@ -24,25 +25,20 @@ function rename_songs()
 	for i = 5001, 5050 do
 		local h = gma.show.getobj.handle(source .. " " .. i)
 		local label = gma.show.getobj.label(h)
-		gma.echo("Current label: ", label)
-		gma.user.setvar("philsong" .. j, string.format("%s", label))
+		local seq_label = label .. user_suffix
+		gma.user.setvar("philsong" .. j, string.format("%s", seq_label))
 		for _, v in pairs(objects_to_rename) do
-			if v == source then
-				goto continue
-			end
-			local cmd = ""
+			local cmd = 'label %s %d "%s"'
 			if v == "view" then
-				cmd = string.format('label %s %d "%s"', v, i + 1000, label)
-				-- cmd = "label " .. v .. " " .. i + 1000 .. ' "' .. label .. '"'
+				cmd = cmd:format(v, i + 1000, label)
+			elseif v == "seq" then
+				cmd = cmd:format(v, i, seq_label)
 			else
-				cmd = string.format('label %s %d "%s"', v, i, label)
-				-- cmd = "label " .. v .. " " .. i .. ' "' .. label .. '"'
+				cmd = cmd:format(v, i, label)
 			end
 			-- gma.echo("Sending command: "..cmd)
 			gma.cmd(cmd)
-			::continue::
 		end
 		j = j + 1
 	end
 end
-
