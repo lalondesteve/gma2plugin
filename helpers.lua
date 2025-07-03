581b7bfa40dbb61b41b466ea1d1d5d4d6d49bb0a
steve <steve@luzstudio.net>
2025-07-03 17:06:23 -0400

    moved single function scripts to helpers file

--- helpers.lua
+++ helpers.lua
@@ -1,3 +1,20 @@
+--[[
+multiple helper functions 
+By Steve Lalonde
+From https://github.com/lalondesteve/gma2plugin
+--]]
+
+function set_clip_speed(clip_tempo)
+	if tonumber(clip_tempo) == nil then
+		gma.gui.msgbox("Invalid Input", "You must enter a numeric value")
+		return
+	end
+
+	local speed = gma.user.getvar("PhilTempo") / clip_tempo
+	-- gma.feedback('Speed: ', speed)
+	gma.cmd('Attribute "speed" at ' .. speed)
+end
+
 function rename_songs()
 	local user_suffix = " _vx"
 	local objects_to_rename = {
